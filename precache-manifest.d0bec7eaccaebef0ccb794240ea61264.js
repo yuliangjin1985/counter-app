0e349274ad8c5da855508212acd88141badf2638
yuliang Jin <yuliangjin@yuliangs-MacBook-Air.local>
2019-03-11 20:50:32 -0500

    Updates

---
+++ precache-manifest.d0bec7eaccaebef0ccb794240ea61264.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/counter-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d4fdbecadf95013603d8",
+    "url": "/counter-app/static/js/runtime~main.d4fdbeca.js"
+  },
+  {
+    "revision": "47721e32a16405c6ad9a",
+    "url": "/counter-app/static/js/main.47721e32.chunk.js"
+  },
+  {
+    "revision": "23376613c83c5d9a3827",
+    "url": "/counter-app/static/js/1.23376613.chunk.js"
+  },
+  {
+    "revision": "47721e32a16405c6ad9a",
+    "url": "/counter-app/static/css/main.2f6ad355.chunk.css"
+  },
+  {
+    "revision": "23376613c83c5d9a3827",
+    "url": "/counter-app/static/css/1.709ce263.chunk.css"
+  },
+  {
+    "revision": "a7fbe972f1f7e4de3b1db67b804a83ca",
+    "url": "/counter-app/index.html"
+  }
+];
